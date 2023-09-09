8a25b527a049beb4f29d2a6bb8f361b5b277c1a5
Isaque <isaque.santos@ufms.br>
2023-09-09 03:58:05 -0400

    Cards dos blogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
